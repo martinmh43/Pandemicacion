fb0fc9d213e60d774c02be599eb55535656620a4
Martín <martinmh43@educastur.es>
2024-10-22 17:31:22 +0200

    perfilUser

---
+++ src/app/perfil/perfil-user/perfil-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-user.component.html',
+  styleUrl: './perfil-user.component.scss'
+})
+export class PerfilUserComponent {
+
+}
