86ba24923fe4ba475c6fdfad276453bcf837e8fc
adrianml14 <adrianml14@educastur.es>
2024-11-05 16:44:44 +0100

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
         AppComponent,
-
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule 
+        AppRoutingModule,
+        CommonModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
