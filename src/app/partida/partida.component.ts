38524f358c47cef85db88451b8f234105daf7c03
adrianml14 <adrianml14@educastur.es>
2024-11-30 21:42:23 +0100

    viñeta ciudad

--- src/app/partida/partida.component.ts
+++ src/app/partida/partida.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-partida',
@@ -12,7 +13,8 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class PartidaComponent {
   ciudades: any[] = [];
-  puntos: any[] = []; 
+  puntos: any[] = [];
+  ciudadSeleccionada: any = null;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -25,34 +27,35 @@ export class PartidaComponent {
   }
 
   procesarDatosCiudades() {
-    this.http.get<any[]>('assets/ciudades.json').subscribe(data => { 
-      console.log('Datos de ciudades:', data); 
+    this.http.get<any[]>('assets/ciudades.json').subscribe(data => {
+      console.log('Datos de ciudades:', data);
       this.ciudades = data.map(ciudad => ({
         nombre: ciudad.nombre.trim(),
         idEnfermedad: ciudad.idEnfermedad,
         coordenadasX: ciudad.coordenadas.x,
         coordenadasY: ciudad.coordenadas.y,
-        conexiones: ciudad.conexiones
+        conexiones: ciudad.conexiones,
+        enfermedades: ciudad.enfermedades 
       }));
 
       this.puntos = this.ciudades.map(ciudad => ({
-        ciudad: ciudad.nombre,  
-        x: ciudad.coordenadasX, 
-        y: ciudad.coordenadasY  
+        ciudad: ciudad.nombre,
+        x: ciudad.coordenadasX,
+        y: ciudad.coordenadasY,
+        enfermedades: ciudad.enfermedades
       }));
 
-      console.log('Puntos procesados:', this.puntos); 
+      console.log('Puntos procesados:', this.puntos);
     }, error => {
       console.error('Error al cargar ciudades', error);
     });
   }
 
-  onPuntoClick(ciudad: string) {
-    console.log(`Has hecho clic en: ${ciudad}`); 
+  onPuntoClick(punto: any) {
+    this.ciudadSeleccionada = punto;
+    console.log(`Has hecho clic en: ${punto.ciudad}`);
   }
-
-
-
-
-
+  cerrarInfo() {
+    this.ciudadSeleccionada = null;
+  }
 }
